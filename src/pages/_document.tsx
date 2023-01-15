ae92ccc6ef7cbc5bb0626a12776d77ba8fb0ae6d
YiDaoJ <jcxsusan@gmail.com>
2023-01-15 21:50:04 +0100

    add customized document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,37 @@
+import clsx from "clsx";
+import Document, {
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body
+          className={clsx(
+            "bg-gray-50 text-gray-800",
+            "dark:bg-gray-900 dark:text-gray-50"
+          )}
+        >
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
